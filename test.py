338c9ac513e73781ef39aa4b83869bf3a12004ad
carlos <ccarbonell@gmail.com>
2025-04-16 15:17:56 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,25 +2,42 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # SPDX-FileCopyrightText: 2023 Kent Gibson <warthog618@gmail.com>
 
-"""Minimal example of reading a single line."""
+"""Minimal example of toggling a single line."""
 
 import gpiod
+import time
 
-from gpiod.line import Direction
+from gpiod.line import Direction, Value
 
 
-def get_line_value(chip_path, line_offset):
+def toggle_value(value):
+    if value == Value.INACTIVE:
+        return Value.ACTIVE
+    return Value.INACTIVE
+
+
+def toggle_line_value(chip_path, line_offset):
+    value_str = {Value.ACTIVE: "Active", Value.INACTIVE: "Inactive"}
+    value = Value.ACTIVE
+
     with gpiod.request_lines(
         chip_path,
-        consumer="get-line-value",
-        config={line_offset: gpiod.LineSettings(direction=Direction.INPUT)},
+        consumer="toggle-line-value",
+        config={
+            line_offset: gpiod.LineSettings(
+                direction=Direction.OUTPUT, output_value=value
+            )
+        },
     ) as request:
-        value = request.get_value(line_offset)
-        print("{}={}".format(line_offset, value))
+        while True:
+            print("{}={}".format(line_offset, value_str[value]))
+            time.sleep(1)
+            value = toggle_value(value)
+            request.set_value(line_offset, value)
 
 
 if __name__ == "__main__":
     try:
-        get_line_value("/dev/gpiochip0", 5)
+        toggle_line_value("/dev/gpiochip0", 5)
     except OSError as ex:
         print(ex, "\nCustomise the example configuration to suit your situation")
